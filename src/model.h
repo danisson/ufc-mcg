0cc80899a393e30dd0e87ef828047bbde8dc6c3a
Daniel Augusto <danisson10@gmail.com>
2016-09-13 00:02:35 +0100

    Organizando umas coisinhas

--- src/model.h
+++ src/model.h
@@ -1,28 +1,36 @@
 #ifndef _H_MODEL
 #define _H_MODEL
 #include <glm/glm.hpp>
+#include <array.h>
 
 namespace tnw {
 
 class Model {
 public:
-	// Função que será usada pra desenhar o modelo
 	virtual void draw() = 0;
-	// Funções geométricas
-	virtual void translate(const glm::vec3& dv);
-	// Operações booleanas
-	virtual void bool_and(const Model& y);
-	// Análise geométrica
+	// Geometric operations
+	virtual void translate(const glm::vec3& dv) = 0;
+	// Boolean operations
+	virtual void bool_and(const Model& y) = 0;
+	// Geometric analysis
 	virtual double volume() = 0;
 };
 
+namespace octree {
+	enum class Color {
+		white, black, gray;
+	};
 
-using Classificador = std::function<bool(glm::mat3x4)>;
-class Octree : public Model {
-public:
-	Octree();
-	~Octree();
-};
+	struct Tree {
+		Color color;
+		std::array<Tree*,8> children;
+	};
+	struct BoundingBox;
+
+	using Classifier = std::function<Color(const BoundingBox&)>;
+} // namespace tnw::octree
+
+class Octree : public Model;
 
 } // namespace tnw
 #endif
