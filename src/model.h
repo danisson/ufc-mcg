2fd25c2dae2ac6275ecb1d711900ea981b084568
Daniel Augusto <danisson10@gmail.com>
2016-09-25 20:53:55 +0100

    Separado arquivo para octree.h

--- src/model.h
+++ src/model.h
@@ -4,6 +4,7 @@
 #include <array>
 #include <string>
 #include <functional>
+#include "octree.h"
 
 namespace tnw {
 
@@ -18,75 +19,7 @@ public:
 	virtual double volume() = 0;
 };
 
-namespace octree {
-	enum class Color {
-		white, black, gray
-	};
-
-	struct BoundingBox
-	{
-		glm::vec3 corner;
-		float depth;
-		BoundingBox(glm::vec3 _corner, float _depth);
-		void draw() const;
-		BoundingBox operator[](size_t position) const;
-		glm::vec3 getVertice(unsigned int i) const;
-		glm::vec3 getCenter() const;
-	};
-
-	using Classifier = std::function<Color(const BoundingBox&)>;
-
-	struct Tree {
-		Color color;
-		std::array<Tree*,8> children;
-		Tree* parent;
-		float drawColor[3];
-
- 		// Constructor for intermediate nodes
-		Tree(std::array<Tree*,8> children, Tree* parent = nullptr);
-		// Constructor for leafs
-		Tree(Tree* parent = nullptr);
-		//Drawing function
-		void draw(const BoundingBox& bb);
-
-		Tree*& operator[](size_t i);
-
-		std::string serialize() const;
-		void classify(Classifier function, BoundingBox bb, unsigned int maxDepth, unsigned int currDepth);
-	};
-
-	class Sphere {
-	public:
-		Sphere(glm::vec3 center, float radius);
-		Color operator()(const BoundingBox&);
-	private:
-		glm::vec3 center;
-		float radius;
-	};
-
-	class Box {
-	public:
-		Box(glm::vec3 center, float length, float depth, float height);
-		Color operator()(const BoundingBox&);
-	private:
-		glm::vec3 center;
-		float length, depth, height;
-	};
-
-	class Cilinder {
-	public:
-		Cilinder(glm::vec3 inferiorPoint, float height, float radius);
-		Color operator()(const BoundingBox&);
-	private:
-		glm::vec3 inferiorPoint;
-		float height, radius;
-	};
-
-	// Builds a tree from a file, stops reading until end of line
-	Tree make_from_file(FILE* f);
-} // namespace tnw::octree
-
-class Octree : public Model { };
+class Octree : public Model {};
 
 } // namespace tnw
 #endif
